e09c4b507981eb99d45eb7ce3ffa37bc1cc70f6a
Zachary Huang <zdhuang@ucsd.edu>
2026-04-15 01:46:13 -0700

    Add sidekick section

---
+++ src/components/Sidekick.tsx
@@ -0,0 +1,14 @@
+import styles from './Sidekick.module.scss';
+
+export default function Sidekick() {
+  return (
+    <section>
+      <div className={styles.sideKick}>
+        <h1>
+          Bear Gardens are daytime carnivals where students can chill with free food and play games to win some bear-y cool prizes with our mascot, ASCE Bear. 
+        </h1>
+        <p>As a play on “Biergarten”, Bear Garden serves free beer to students over 21 who present a valid ID, but the fun is open to all UCSD undergraduate students! Each Bear Garden also has a special theme with exclusive, themed stickers and merchandise. Come out for a paw-some time!</p>
+      </div>
+    </section>
+  );
+}
