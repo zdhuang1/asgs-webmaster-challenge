c3221672c7d42dbe1f884a106b9f0294facaff39
Zachary Huang <zdhuang@ucsd.edu>
2026-04-14 22:40:35 -0700

    Initial commit: Setup Vite, React, TS, and Tailwind

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/asgs-webmaster-challenge',
+})
